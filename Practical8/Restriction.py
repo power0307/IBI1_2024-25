f930dafc555ce3193eb8c853737683916abba037
power0307 <yingbo.24@intl.zju.edu.cn>
2025-04-09 08:09:32 +0100

    4.9

--- Practical8/Restriction.py
+++ Practical8/Restriction.py
@@ -9,6 +9,12 @@ def Restriction_enzyme_cut_sites(sequence, restriction_enzyme):
     for i in range(len(sequence) - enzyme_length + 1): #from 0 to the len(sequence) - enzyme_length + 1
         if sequence[i:i + enzyme_length] == restriction_enzyme: # check if the part of sequence == restriction_enzyme
             sites.append(i+1) # write the place
-    print(sites)
-    return sites
-Restriction_enzyme_cut_sites("ATCGTTCG","TCG")
+    if sites == []:
+        print("not found")
+        return
+    else:
+      print(sites)
+      return sites
+Restriction_enzyme_cut_sites("ATCGTTCG","TCG") # find the 2 and 6 is the start of TCG
+Restriction_enzyme_cut_sites("ATCGTTCGYS","TCG") # error
+Restriction_enzyme_cut_sites("ATCGTTCG","TCGA") # 
