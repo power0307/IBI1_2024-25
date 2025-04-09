52d288a6cd7281528af8e59cc8f78aa6eda63c2c
power0307 <yingbo.24@intl.zju.edu.cn>
2025-04-09 08:19:26 +0100

    4.9

--- Practical7/spliced_tata_genes.py
+++ Practical7/spliced_tata_genes.py
@@ -1,5 +1,7 @@
+import re
+
 file_path = r"C:\Users\F\Desktop\IBI1\IBI1_2024-25\Practical7\Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa"
-tata_patterns = ["GTAG", "GCAG", "ATAC"] #give the pattern
+tata_patterns = ["GTAG", "GCAG", "ATAC"]  # give the pattern
 a = input("Choose GTAG or GCAG or ATAC: ").strip().upper() 
 if a not in tata_patterns:
     print("Invalid choice. Please enter GTAG, GCAG, or ATAC.")
@@ -9,12 +11,12 @@ with open(file_path, "r") as file, open(output_path, "w") as newfile:
     current_header = ""
     current_sequence = []
     
-    for line in file: # check line in the file one by one
-        if line.startswith(">"): # find the ">" in the line
-            if current_header and current_sequence: # check if these two things are full
+    for line in file:  # check line in the file one by one
+        if line.startswith(">"):  # find the ">" in the line
+            if current_header and current_sequence:  # check if these two things are full
                 full_sequence = "".join(current_sequence)
-                if a in full_sequence:  # Only check the user-selected pattern
-                    count = full_sequence.count(a)
+                count = len(re.findall(a, full_sequence))
+                if count > 0:
                     newfile.write(f"{current_header[:8]}\n")
                     newfile.write(f"Number of {a} : {count}\n")
                     newfile.write(f"{full_sequence}\n\n")
@@ -27,8 +29,8 @@ with open(file_path, "r") as file, open(output_path, "w") as newfile:
     # Handle the last gene
     if current_header and current_sequence:
         full_sequence = "".join(current_sequence)
-        if a in full_sequence:
-            count = full_sequence.count(a)
+        count = len(re.findall(a, full_sequence))
+        if count > 0:
             newfile.write(f"{current_header[:8]}\n")
             newfile.write(f"Number of {a} : {count}\n")
             newfile.write(f"{full_sequence}\n\n")
